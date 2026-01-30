a1c8e72c7eb43a6e7accf535aa16677d43467cff
Ausin Neveau <austinneveau93@gmail.com>
2026-01-30 10:08:54 -0500

    final ui fix for today

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -12,6 +12,7 @@ import Container from "@mui/material/Container";
 import everbankSite_IMG from "../../assets/images/everbank-site.png";
 import nuvodrawsSite_IMG from "../../assets/images/nuvodraws-site.png";
 import leafsAdventure_IMG from "../../assets/images/leafsAdventure.png";
+import recallisApp_IMG from "../../assets/images/recallis-site.png";
 import internal_IMG from "../../assets/images/internal.png";
 
 export default function Projects() {
@@ -26,13 +27,13 @@ export default function Projects() {
             </Typography>
           </Grid>
           <Grid item xs={12} lg={3}>
-            <Card sx={{ height: "100%" }}>
+            <Card sx={{ height: "100%", display: "flex", flexDirection: "column" }}>
               <CardMedia
                 sx={{ height: 176 }}
                 image={everbankSite_IMG}
                 title="green iguana"
               />
-              <CardContent>
+              <CardContent sx={{ flexGrow: 1 }}>
                 <Typography gutterBottom variant="h5" component="div">
                   EverBank
                 </Typography>
@@ -52,13 +53,13 @@ export default function Projects() {
             </Card>
           </Grid>
           <Grid item xs={12} lg={3}>
-            <Card sx={{ height: "100%" }}>
+            <Card sx={{ height: "100%", display: "flex", flexDirection: "column" }}>
               <CardMedia
                 sx={{ height: 176 }}
                 image={nuvodrawsSite_IMG}
                 title="green iguana"
               />
-              <CardContent>
+              <CardContent sx={{ flexGrow: 1 }}>
                 <Typography gutterBottom variant="h5" component="div">
                   Art Portfolio
                 </Typography>
@@ -78,55 +79,52 @@ export default function Projects() {
             </Card>
           </Grid>
           <Grid item xs={12} lg={3}>
-            <Card sx={{ height: "100%" }}>
+            <Card sx={{ height: "100%", display: "flex", flexDirection: "column" }}>
               <CardMedia
                 sx={{ height: 191 }}
-                image={leafsAdventure_IMG}
+                image={recallisApp_IMG}
                 title="green iguana"
               />
-              <CardContent>
+              <CardContent sx={{ flexGrow: 1 }}>
                 <Typography gutterBottom variant="h5" component="div">
-                  Leafs Adventure
+                  Recallis
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
-                  Leafs Adventure was a mobile platformer game I developed for
-                  unity as a senior project in college. This was a really
-                  enjoyable programming experience for me as gaming has always
-                  been a hobby of mine. The game was inspired by classic games
-                  like Super Mario bros and the Legend of Zelda.
+                  Recallis was one of my first projects I devloped in order to
+                  learn more about OpenAI and their APIs. Recallis is an
+                  AI-powered flashcard generator that helps users create
+                  personalized study materials quickly and easily. By leveraging
+                  advanced natural language processing techniques, Recallis
+                  analyzes user-provided content and generates relevant
+                  flashcards to enhance learning and retention.
                 </Typography>
               </CardContent>
               <CardActions>
                 <Button size="small">
-                  <a href="https://www.leafs-adventure.com/">Leafs Adventure</a>
+                  <a href="https://www.app.recallis.com/">Recallis.com</a>
                 </Button>
               </CardActions>
             </Card>
           </Grid>
           <Grid item xs={12} lg={3}>
-            <Card sx={{ height: "100%" }}>
+            <Card sx={{ height: "100%", display: "flex", flexDirection: "column" }}>
               <CardMedia
-                sx={{ height: 191 }}
+                sx={{ height: 140 }}
                 image={internal_IMG}
                 title="green iguana"
               />
-              <CardContent>
+              <CardContent sx={{ flexGrow: 1 }}>
                 <Typography gutterBottom variant="h5" component="div">
-                  Internal
+                  Internal App
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
                   A large number of the applications I have supported have been
                   internal applications. Most of these are .NET web services
                   that support the day to day business functionality. I've also
-                  worked internally with Netlify, OpenShift, SQL Server, Azure DevOps,
-                  TFS, Git and more.
+                  worked internally with Netlify, OpenShift, SQL Server, Azure
+                  DevOps, TFS, Git and more.
                 </Typography>
               </CardContent>
-              <CardActions>
-                <Button size="small">
-                  <a href="https://www.internal.com/">Internal.com</a>
-                </Button>
-              </CardActions>
             </Card>
           </Grid>
         </Grid>
